4a4131d0e21f9237d56905ce2c5a5200d417dcd1
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-08-16 23:47:17 -0400

    Slightly simplified JS password matching function

--- script.js
+++ script.js
@@ -14,41 +14,40 @@ let pwd = document.querySelector('#pwd');
 let pwdConfirm = document.querySelector('#pwd-confirm');
 
 let submit = document.querySelector('#submit');
-/*
-function matching(field1, field2) {
-    let string1 = field1.value;
-    let string2 = field2.value;
-    if (string1 == '' || string2 == '') {
-        field1.classList.remove("error");
-        field1.classList.remove("match");
-        field2.classList.remove("error");
-        field2.classList.remove("match");
-        field1.closest('p').classList.remove("error");
+
+function matching() {
+
+    if (pwd.value == '' || pwdConfirm.value == '') {
+        pwd.classList.remove("error");
+        pwd.classList.remove("match");
+        pwdConfirm.classList.remove("error");
+        pwdConfirm.classList.remove("match");
+        pwd.closest('p').classList.remove("error");
         submit.setAttribute("disabled", true);
     }
-    */
     
-    else if (string1 == string2) {
-        field1.classList.remove("error");
-        field1.classList.add("match");
-        field2.classList.remove("error");
-        field2.classList.add("match");
-        field1.closest('p').classList.remove("error");
-        field1.closest('p').classList.add("match");
+    else if (pwd.value == pwdConfirm.value) {
+        pwd.classList.remove("error");
+        pwd.classList.add("match");
+        pwdConfirm.classList.remove("error");
+        pwdConfirm.classList.add("match");
+        pwd.closest('p').classList.remove("error");
+        pwd.closest('p').classList.add("match");
         submit.removeAttribute("disabled");
 
     }
 
     else {
-        field1.classList.remove("match");
-        field1.classList.add("error");
-        field2.classList.remove("match");
-        field2.classList.add("error");
-        field1.closest('p').classList.remove("match");
-        field1.closest('p').classList.add("error");
+        pwd.classList.remove("match");
+        pwd.classList.add("error");
+        pwdConfirm.classList.remove("match");
+        pwdConfirm.classList.add("error");
+        pwd.closest('p').classList.remove("match");
+        pwd.closest('p').classList.add("error");
         submit.setAttribute("disabled", true);
     }
 }
 
-pwd.addEventListener("input", () => {matching(pwd, pwdConfirm)});
-pwdConfirm.addEventListener("input", () => {matching(pwd, pwdConfirm)});
+
+pwd.addEventListener("input", () => {matching()});
+pwdConfirm.addEventListener("input", () => {matching()});
